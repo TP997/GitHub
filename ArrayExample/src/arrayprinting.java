69f9f8a7522091fa7b4947e0a792418e5e27c5d1
TP997 <pthanuja997.com>
2023-03-07 18:11:10 -0800

    array creation

---
+++ ArrayExample/src/arrayprinting.java
@@ -0,0 +1,9 @@
+public class arrayprinting {
+    public static void main(String[] args){
+        int a[]={1,18,3,19,5};
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+
+        }
+    }
+}
